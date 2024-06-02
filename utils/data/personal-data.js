7f26de5ae826adeeca7a88a1f5d446cda000c823
Paulo Ricardo <103902227+PauloRWM@users.noreply.github.com>
2024-06-02 14:50:15 -0300

    first commit

---
+++ utils/data/personal-data.js
@@ -0,0 +1,18 @@
+export const personalData = {
+  whatsapp:"https://wa.me/5598991144288",
+  name: "Paulo Ricardo",
+  profile: '/profile.png',
+  designation: "Desenvolvedor Web",
+  description: "Meu nome é Paulo Ricardo, sou desenvolvedor Web, trabalho com as principais linguagens de Backend,como PHP e JS(NODE), tambem tenho experiencia com CMS (Wordpress) onde atuei tanto com o Elementor como no desenvolvimento de plugins.",
+  email: 'paulox.tec@gmail.com',
+  phone: '+5598991144288',
+  address: 'Rosário - MA, Brasil',
+  github: 'https://github.com/PauloRWM',
+  facebook: '',
+  linkedIn: 'https://www.linkedin.com/in/paulo-ricardo-waquim-medeiros-6348391b9/',
+  twitter: '',
+  stackOverflow: '',
+  leetcode: "",
+  devUsername: "PauloRWM",
+  resume: "https://drive.google.com/file/d/1PIWl5WOdC8WpL6uT9nLqgLQvDNFYTIvH/view?usp=drive_link"
+}
